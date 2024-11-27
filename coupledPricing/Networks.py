7a30bc524aca474c184bdae10e36ba96ec8dea36
Zakaria Bensaid <zakbensaid654@gmail.com>
2024-11-27 12:58:00 +0100

    Add files via upload

--- coupledPricing/Networks.py
+++ coupledPricing/Networks.py
@@ -2,20 +2,22 @@ import sys
 import tensorflow as tf
 from tensorflow.keras import layers
 
-#Neural Network
+#Neural networks
 class Net( tf.keras.Model):
     def __init__( self, bY0, ndimOut, nbNeurons, activation= tf.nn.tanh):
         super().__init__()
         self.nbNeurons = nbNeurons
-        self.name_ = "FeedForward"
+        self.name_ = "FeedForwardANd0"
         self.ndimOut = ndimOut
         self.ListOfDense =  [layers.Dense( nbNeurons[i],activation= activation, kernel_initializer= tf.keras.initializers.GlorotNormal())  for i in range(len(nbNeurons)) ]+[layers.Dense(self.ndimOut, activation= None, kernel_initializer= tf.keras.initializers.GlorotNormal())]
         if (bY0 ==1):
-            self.Y0= tf.Variable(tf.keras.initializers.GlorotNormal()([]),  trainable = True, dtype=tf.float32)
+          self.Y0 = tf.Variable(tf.keras.initializers.GlorotNormal()([]),  trainable = True, dtype=tf.float32)
 
 
     def call(self,inputs):
-        x = inputs 
+        x = inputs
         for layer in self.ListOfDense:
             x = layer(x)
+        if len(x.shape) == 3 :
+          return [x[:,:,i] for i in range(self.ndimOut)]
         return [x[:,i] for i in range(self.ndimOut)]
